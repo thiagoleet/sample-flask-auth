a9aa7401bee14c870ddfaf2d04bd23ffcbb3a21f
Thiago Leite <thiagofmleite@gmail.com>
2024-04-26 17:01:43 -0300

    chore: dealing with login

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from database import db
+from loginmanager import login_manager
+from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+db.init_app(app=app)
+login_manager.init_app(app=app)
+
+# view login
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        # Login
+        return jsonify({"message": "Login com sucesso"}), 200
+    else:
+        return jsonify({"message": "Credenciais inválidas"}), 400
 
 
 @app.route('/hello-world', methods=['GET'])
@@ -15,5 +33,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
-else:
-    app.run(debug=False)
