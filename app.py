d63a0f691ade97965ab17fda000bd9742de05f4e
Thiago Leite <thiagofmleite@gmail.com>
2024-04-26 18:45:26 -0300

    chore: creating user route (POST)

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
-
 from models.user import User
 
 app = Flask(__name__)
@@ -42,6 +41,21 @@ def login():
 def logout():
     logout_user()
     return jsonify({"message": "Logout com sucesso"}), 200
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": f"Usuário {username} criado com sucesso"}), 201
+
+    return jsonify({"message": "Dados inválidos"}), 400
 
 
 @app.route('/hello-world', methods=['GET'])
